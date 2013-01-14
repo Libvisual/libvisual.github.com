5d58f06c0bbd95e445fe3801f2baae4884f517f4
Chong Kai Xiong <kaixiong@codeleft.sg>
2013-01-15 04:05:38 +0800

    Replace CSS reset with normalize and use Compass to generate stylesheets

---
+++ compass.rb
@@ -0,0 +1,9 @@
+css_dir = 'css'
+sass_dir = 'css'
+images_dir = 'css/images'
+javascripts_dir = 'js'
+
+output_style = :expanded
+relative_assets = true
+line_comments = false
+preferred_syntax = :sass
